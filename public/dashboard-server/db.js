05b9ebe2df4f80096babc7009a6544a60c2c29c3
Johannes Haller <johanneshaller96@gmail.com>
2025-07-20 13:15:36 +0200

    db testing

--- public/dashboard-server/db.js
+++ public/dashboard-server/db.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  ssl: { rejectUnauthorized: false }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false // wichtig für Render
+  }
 });
 
 module.exports = pool;
